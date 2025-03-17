b59b3cc57a4cdf08f1f8b62eb6c0bce780887d53
DhananjayaKumara99 <it21184208@my.sliit.lk>
2025-03-17 11:05:19 +0530

    minor chnage

--- lib/select_fruits_screen.dart
+++ lib/select_fruits_screen.dart
@@ -13,106 +13,106 @@ class _SelectFruitsScreenState extends State<SelectFruitsScreen> {
     // Fruits
     FruitItem(
       name: 'Apple',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/415/415682.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.red,
     ),
     FruitItem(
       name: 'Banana',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/3143/3143645.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.yellow,
     ),
     FruitItem(
       name: 'Orange',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/415/415733.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.orange,
     ),
     FruitItem(
       name: 'Grapes',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/2224/2224241.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.purple,
     ),
     FruitItem(
       name: 'Strawberry',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/590/590685.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.red,
     ),
     FruitItem(
       name: 'Mango',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/6866/6866569.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.amber,
     ),
     FruitItem(
       name: 'Pineapple',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/3137/3137038.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.amber.shade800,
     ),
     FruitItem(
       name: 'Watermelon',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/874/874997.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.green,
     ),
 
     // Additional Fruits
     FruitItem(
       name: 'Kiwi',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/2224/2224118.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.green.shade800,
     ),
     FruitItem(
       name: 'Papaya',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/6866/6866522.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.orange.shade300,
     ),
     FruitItem(
       name: 'Pomegranate',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/6866/6866536.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.red.shade800,
     ),
 
     // Vegetables
     FruitItem(
       name: 'Tomato',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/1412/1412511.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.red,
     ),
     FruitItem(
       name: 'Potato',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/2224/2224187.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.brown.shade300,
     ),
     FruitItem(
       name: 'Onion',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/2224/2224171.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.purple.shade200,
     ),
     FruitItem(
       name: 'Carrot',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/1147/1147801.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.orange.shade700,
     ),
     FruitItem(
       name: 'Cucumber',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/2224/2224112.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.green.shade600,
     ),
     FruitItem(
       name: 'Cabbage',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/2224/2224229.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.green,
     ),
     FruitItem(
       name: 'Broccoli',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/1147/1147832.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.green.shade800,
     ),
     FruitItem(
       name: 'Eggplant',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/2224/2224116.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.deepPurple,
     ),
     FruitItem(
       name: 'Bell Pepper',
-      imageUrl: 'https://cdn-icons-png.flaticon.com/512/2224/2224098.png',
+      imagePath: 'assets/images/apple.png',
       color: Colors.red.shade600,
     ),
   ];
@@ -127,44 +127,40 @@ class _SelectFruitsScreenState extends State<SelectFruitsScreen> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(height: 20),
-            // Agriconnect aligned to left
-            Container(
-              alignment: Alignment.centerLeft,
-              child: RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'Agri',
-                      style: TextStyle(
-                        color: Colors.green,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30, // Increased font size
-                      ),
+            // Changed from Center to align left
+            RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'Agri',
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30, // Increased font size
                     ),
-                    TextSpan(
-                      text: 'connect',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30, // Increased font size
-                      ),
+                  ),
+                  TextSpan(
+                    text: 'connect',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30, // Increased font size
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
             const SizedBox(height: 20),
-            // "Set up your account" aligned to left
+            // Changed from Center to align left
             const Text(
               'Set up your account',
               style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.left,
             ),
             const SizedBox(height: 20),
-            // "Select your produce" centered
+            // Keeping this centered
             const Center(
               child: Text(
-                'Select your produce',
+                'Select your fruits',
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 textAlign: TextAlign.center,
               ),
@@ -221,13 +217,13 @@ class _SelectFruitsScreenState extends State<SelectFruitsScreen> {
 
 class FruitItem {
   final String name;
-  final String imageUrl;
+  final String imagePath;
   final Color color;
   bool isSelected;
 
   FruitItem({
     required this.name,
-    required this.imageUrl,
+    required this.imagePath,
     required this.color,
     this.isSelected = false,
   });
@@ -259,27 +255,7 @@ class FruitSelectionTile extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.network(
-              fruit.imageUrl,
-              height: 40,
-              width: 40,
-              loadingBuilder: (context, child, loadingProgress) {
-                if (loadingProgress == null) return child;
-                return Center(
-                  child: CircularProgressIndicator(
-                    value:
-                        loadingProgress.expectedTotalBytes != null
-                            ? loadingProgress.cumulativeBytesLoaded /
-                                loadingProgress.expectedTotalBytes!
-                            : null,
-                    color: fruit.color,
-                  ),
-                );
-              },
-              errorBuilder: (context, error, stackTrace) {
-                return Icon(Icons.error, color: fruit.color);
-              },
-            ),
+            Image.asset(fruit.imagePath, height: 40, width: 40),
             const SizedBox(height: 8),
             Text(
               fruit.name,
